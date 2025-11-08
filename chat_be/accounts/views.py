375fb1ca28abf6a123eac5974890f17fa8c4c364
Amit Kumar <amitalwarpvt@gmail.com>
2025-11-08 15:14:09 +0530

    Initial commit

---
+++ chat_be/accounts/views.py
@@ -0,0 +1,41 @@
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "user": serializer.data,
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+
+class LoginView(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({"error":"Invalid email or password"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = authenticate(username=user.username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            })
+        else:
+            return Response({"error":"Invalid email or password"}, status=status.HTTP_400_BAD_REQUEST)
